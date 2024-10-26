8659c5c1be0dd51bcd511f72b85c581bac6d5fd0
tad-iizuka <tad.iizuka@gmail.com>
2024-10-26 15:47:40 +0900

    change

--- header/so_long.h
+++ header/so_long.h
@@ -6,7 +6,24 @@
 /*   By: tiizuka <tiizuka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 17:39:24 by tiizuka           #+#    #+#             */
-/*   Updated: 2024/10/25 17:39:30 by tiizuka          ###   ########.fr       */
+/*   Updated: 2024/10/26 14:57:32 by tiizuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include	<stdio.h>
+# include	<string.h>
+# include	<stdlib.h>
+# include	<X11/Xlib.h>
+# include	<sys/ipc.h>
+# include	<sys/shm.h>
+# include	<X11/extensions/XShm.h>
+# include	"../minilibx-linux/mlx.h"
+# include	"../libft/libft.h"
+
+# define SCREEN_WIDHT 640
+# define SCREEN_HEIGHT 640
+
+#endif
--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,43 +6,32 @@
 /*   By: tiizuka <tiizuka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 17:39:36 by tiizuka           #+#    #+#             */
-/*   Updated: 2024/10/25 18:06:22 by tiizuka          ###   ########.fr       */
+/*   Updated: 2024/10/26 15:03:59 by tiizuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include	<X11/Xlib.h>
-#include	<sys/ipc.h>
-#include	<sys/shm.h>
-#include	<X11/extensions/XShm.h>
-#include	"../minilibx-linux/mlx.h"
+#include	"so_long.h"
 
 int	main(void)
 {
-     void *mlx;//スクリーン接続識別子
-     void *mlx_win;//ウィンドウ識別子
-     int i;
-     int j;
-     int width = 1000; //ウィンドウのサイズ
-     int height = 1000;
+	void	*mlx;
+	void	*mlx_win;
+	int		i;
+	int		j;
 
-     mlx = mlx_init();//mlxの機能を使う前に一度呼び出す必要がある
-     mlx_win = mlx_new_window(mlx, width, height,"test");
-		// mlx_win = mlx_new_window(スクリーン識別子, width, height,"test");
-		//座標は左上が原点、矢印はx軸が右、y軸が下向きになる
-		//でも与えられる図形の座標は一般的な向きの座標のためあとで座標変換が必要となる
-    	i= 0;
-		while (i < width)
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, SCREEN_WIDHT, SCREEN_HEIGHT, "test");
+	i = 0;
+	while (i < SCREEN_WIDHT)
+	{
+		j = 0;
+		while (j < SCREEN_WIDHT)
 		{
-				j = 0;
-				while (j < height)
-				{
-						mlx_pixel_put(mlx, mlx_win, i, j, 127);
-						j++;
-				}
-				i++;
+			mlx_pixel_put(mlx, mlx_win, i, j, 127);
+			j++;
 		}
-     mlx_loop(mlx);//無限ループを作り、その間ウィンドウを表示させ続ける
-     //最初のうちはcntl+Cで強制終了すればおk
-     //クリックした時に終了させるなどのプログラムはゆくゆく書く必要がある
-     return (1);
+		i++;
+	}
+	mlx_loop(mlx);
+	return (1);
 }
