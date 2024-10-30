baab0f782a88a8029d086c0e6d3b52c9fefeeda4
tad-iizuka~ <tad.iizuka@gmail.com>
2024-10-31 05:43:46 +0900

    change

--- header/so_long_error.h
+++ header/so_long_error.h
@@ -6,13 +6,13 @@
 /*   By: tiizuka <tiizuka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 20:19:56 by tiizuka           #+#    #+#             */
-/*   Updated: 2024/10/30 21:19:13 by tiizuka          ###   ########.fr       */
+/*   Updated: 2024/10/31 05:43:11 by tiizuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_ERROR_H
 # define  SO_LONG_ERROR_H
 
-# define ERR_MLX_INIT 0
+# define ERR_MLX_INIT 1000
 
 #endif
