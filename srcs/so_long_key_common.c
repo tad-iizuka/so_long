6162a1870bed14aa47d960b57822919d57ff6e3a
tad-iizuka <tad.iizuka@gmail.com>
2024-11-15 12:41:39 +0900

    p stop

--- srcs/so_long_key_common.c
+++ srcs/so_long_key_common.c
@@ -6,7 +6,7 @@
 /*   By: tiizuka <tiizuka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 12:16:22 by tiizuka           #+#    #+#             */
-/*   Updated: 2024/11/15 12:36:16 by tiizuka          ###   ########.fr       */
+/*   Updated: 2024/11/15 12:40:29 by tiizuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,17 @@ void	set_p(t_texture *txt, int direction, char type)
 	txt->type = 'P';
 	txt->frame = 0;
 	if (type == 'E')
+	{
 		txt->direction = DIR_S;
+		txt->animation = False;
+		txt->max_frame = 0;
+	}
 	else
+	{
 		txt->direction = direction;
-	txt->animation = is_animation(TYPE_P);
-	txt->max_frame = get_frame_max(TYPE_P);
+		txt->animation = is_animation(TYPE_P);
+		txt->max_frame = get_frame_max(TYPE_P);
+	}
 	txt->update = True;
 }
 
